05264ffc918608832aed4a02e196d535f4eeb369
Subbu-battala <subbubattala01@gmail.com>
2023-07-25 10:07:18 +0530

    ex11

--- src/component/Ex11.jsx
+++ src/component/Ex11.jsx
@@ -1,6 +1,32 @@
-import React from "react";
+import React, { useReducer } from "react";
+
+// intial state
+const initState = { num: 0 };
+
+// reducer
+const mathReducer = (state,action) => {
+    //console.log(`action =`. action)
+    if(action === 'plus'){
+        //plus reducer
+        return {num: state.num + 1}
+    }else if(action === 'minus'){
+        //minus reducer
+        if(state.num > 0){
+            return {num: state.num -1}
+        }else{
+            return{num : 0 }
+        }
+    }else if(action === 'clear'){
+        //clear reducer
+        return {num: 0}
+    }else{
+        console.log(`sorry not a valid action`)
+    }
+}
 
 function Ex11(props){
+    //const [sate,dispatcher] = useReducer(reducer,intial-sate)
+    const [state,dispatcher] = useReducer(mathReducer,initState)
     return(
         <div className="container">
             <div className="row">
@@ -8,6 +34,27 @@ function Ex11(props){
                     <h3 className="display-3 text-success">userReducer</h3>
                 </div>
             </div>
+
+            <div className="row">
+                <div className="col-md-12">
+                    <button onClick={() => dispatcher('plus')}  className="btn btn-success float-start">Plus</button>
+                    <button onClick={() => dispatcher('minus')}  className="btn btn-danger float-end">Minus</button>
+                </div>
+            </div>
+
+            <div className="row">
+                <div className="col-md-12 text-center">
+                    <h1 className="text-success display-1">State = {state.num} </h1>
+                </div>
+            </div>
+
+            <div className="row">
+                <div className="col-md-12">
+                    <button onClick={() => dispatcher('clear')}  className="btn btn-warning">Clear</button>
+                </div>
+            </div>
+
+
         </div>
     )
 }
